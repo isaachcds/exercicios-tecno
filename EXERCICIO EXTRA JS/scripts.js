b44adc61868e02e5199d2768d0ee700021da9788
isaachcds <isaachelenohr@gmail.com>
2023-05-10 16:48:08 -0300

    alteração do exercicio extra

--- EXERCICIO EXTRA JS/scripts.js
+++ EXERCICIO EXTRA JS/scripts.js
@@ -1,9 +1,34 @@
+const passwordInput = document.getElementById("password")
+let newPassword = null;
+const button = document.getElementById("btn")
+
+function getPassword() {
+      const chars = "0123456789";
+      const passwordLength = 6;
+      let password = "";
+
+      for (let i = 0; i < passwordLength; i++) {
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+      }
+      return password.toString()
+    }
+
+    button.addEventListener("click", () => {
+      newPassword = getPassword()
+      passwordInput.value = newPassword;
+    })
+
+    
+
+// Validação
 function logar(){
       const login = document.getElementById("login").value;
       const senha = document.getElementById("senha").value;
 
-    if(login == "trends" && senha == "121314"){
-        alert("Sucesso!");
+    if(login == "trends" && senha === newPassword ){
+      alert('Sucesso!')
+      location.replace("https://www.treinaweb.com.br");
      }else{
         alert('Usuario ou senha incorretos');
      }
